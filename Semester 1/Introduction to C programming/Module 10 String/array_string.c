f00f770b5fe6e3fd942b84d56a2db54806cd642b
Md. Shakil Hossain <shakiljusc15@gmail.com>
2023-10-17 08:20:20 +0600

    upload

---
+++ Semester 1/Introduction to C programming/Module 10 String/array_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+ char a[100];
+
+fgets(a,18,stdin);
+ 
+printf("%s",a);
+
+ 
+ return 0;
+}
---
+++ Semester 1/Introduction to C programming/Module 9 Array Oparation/array_copy.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int m;
+    scanf("%d", &m);
+    int b[m];
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    int ans[n + m];
+    for (int i = 0; i < n + m; i++)
+    {
+        ans[i] = arr[i];
+    }
+    int i = n;
+    for (int j = 0; j <= m; j++)
+    {
+        ans[i] = b[j];
+        i++;
+    }
+    for (int i = 0; i < n + m; i++)
+    {
+        printf("%d ", ans[i]);
+    }
+
+    return 0;
+}
